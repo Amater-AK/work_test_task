7cf0feee444f7df910e0e4ca510837cf6ad0279d
Andrey Kupetskov <amater131089@gmail.com>
2022-07-08 16:24:13 +0300

    Added periods creation

--- pages/control.php
+++ pages/control.php
@@ -23,6 +23,81 @@ if($_SESSION["user"]["page_alias"] !== basename(__FILE__)) {
 <?php
 $db = GetDatabase();
 
+// Создание нового периода
+if(isset($_POST["create_period"])) {
+	try {
+		$db->beginTransaction();
+
+		// Обновление текущего периода
+		$stmt = $db->prepare("UPDATE Periods 
+								SET period_end = NOW(), isArchived = 1 
+								WHERE isArchived = 0 
+									AND period_start = (SELECT MAX(period_start) 
+														FROM periods)");
+		$stmt->execute();
+
+		// Добавление нового периода
+		$stmt = $db->prepare("INSERT INTO Periods VALUES()");
+		$stmt->execute();
+
+		// Получение id добавленного периода
+		$stmt = $db->prepare("SELECT id 
+								FROM Periods 
+								WHERE isArchived = 0 
+									AND period_start = (SELECT MAX(period_start) 
+														FROM periods)");
+		$stmt->execute();
+		$data = $stmt->fetch(PDO::FETCH_ASSOC);
+		$new_period_id = $data["id"];
+		$stmt->closeCursor();
+
+		// Создание новых значений для каждого показателя и для каждого проверяющего
+		// Получение id всех проверяющих
+		$stmt = $db->prepare("SELECT U.id 
+								FROM Users AS U, Roles AS R 
+								WHERE U.role_id = R.id 
+									AND R.isChecker = 1 
+								ORDER BY U.id");
+		$stmt->execute();
+		$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		$stmt->closeCursor();
+
+		$checkers_id = array();
+		foreach($data as $value) {
+			$checkers_id[] = $value["id"];
+		}
+
+		// Получение id всех показателей
+		$stmt = $db->prepare("SELECT id 
+								FROM Indicators 
+								ORDER BY id");
+		$stmt->execute();
+		$data = $stmt->fetchAll();
+		$stmt->closeCursor();
+
+		$indicators_id = array();
+		foreach($data as $value) {
+			$indicators_id[] = $value["id"];
+		}
+
+		// Добавление новых значений показателей
+		$vals = array();
+		foreach($checkers_id as $cid) {
+			foreach($indicators_id as $iid) {
+				$vals[] = "(" .$iid ."," .$cid ."," .$new_period_id .")";
+			}
+		}
+
+		$stmt = $db->prepare("INSERT INTO Indicators_values (indicator_id, checker_id, period_id) VALUES " .implode(", ", $vals));
+		$stmt->execute();
+
+		$db->commit();
+	} catch(PDOException $e) {
+		$db->rollBack();
+		echo $e->getMessage();
+	}
+}
+
 // Получение показателей
 $stmt = $db->prepare("SELECT IV.id, U.full_name AS checker, I.title, IV.value 
 						FROM Indicators_values AS IV, Indicators AS I, Users AS U 
@@ -60,4 +135,5 @@ foreach($data as $value) {
 			</tr>
 		<?php endforeach; ?>
 	</table>
+    <p><input type="submit" name="create_period" value="Новый период" /></p>
 </form>
